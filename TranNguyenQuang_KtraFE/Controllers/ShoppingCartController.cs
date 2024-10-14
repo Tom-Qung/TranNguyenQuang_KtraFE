8bfa43c72de37efb93c31b6d31c3d3e5085e73a9
Admin <Admin@ADMIN-PC>
2024-10-14 08:39:56 +0700

    Add project files.

---
+++ TranNguyenQuang_KtraFE/Controllers/ShoppingCartController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TranNguyenQuang_KtraFE.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShoppingCartTiki()
+        {
+            return View();
+        }
+    }
+}
---
+++ TranNguyenQuang_KtraFE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TranNguyenQuang_KtraFE.MvcApplication" Language="C#" %>
